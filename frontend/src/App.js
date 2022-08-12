8a5ce3fe751f8b6cfbb3f2bc04c4e43207841c13
abuhajaar <adambis28@gmail.com>
2022-08-12 18:40:46 +0200

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+
+      <main>List Product</main>
+    </div>
+  );
+}
+
+export default App;
